26e3f435a765976f35c94cc6a34286d1bc0d91d5
Miguel Filipe <miguel.filipe@gmail.com>
2022-12-03 15:50:41 +0000

    add NODE ERC20 contract

---
+++ contracts/NODE.sol
@@ -0,0 +1,10 @@
+// @openzeppelin v4.6.0
+pragma solidity ^0.8.4;
+
+import "./lib/oz/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+
+contract NODE is ERC20PresetMinterPauser {
+
+    constructor() public ERC20PresetMinterPauser("NODE, tugas l33t hax0rs grey hats", "NODE") {}
+
+}
